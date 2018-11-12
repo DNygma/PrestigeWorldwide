ef6fc7a63665291d7b0b11fc6cba7744e4fc62ac
DESKTOP-2SKBU73\Richard Anderson <richard.anderson@p97.com>
2018-11-11 21:24:17 -0600

    Add project files.

---
+++ CreateUsers.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ServerAdmin]
+(
+    [ServerAdmin_ID] INT NOT NULL PRIMARY KEY, 
+    [ServerAdmin_Name] NCHAR(15) NULL, 
+    [ServerAdmin_Number] NCHAR(15) NULL, 
+    [ServerAdmin_ZipCode] NCHAR(15) NULL
+)
+
+CREATE TABLE [dbo].[PortalAdmin]
+(
+    [PortalAdmin_ID] INT NOT NULL PRIMARY KEY, 
+    [PortalAdmin_Name] NCHAR(15) NULL, 
+    [PortalAdmin_Number] NCHAR(15) NULL, 
+    [PortalAdmin_ZipCode] NCHAR(15) NULL
+)
---
+++ PrestigeWorldwide/Controllers/RoleController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using PrestigeWorldwide.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrestigeWorldwide.Controllers
+{   
+    public class RoleController : Controller
+    {
+        ApplicationDbContext _context;
+        // GET: Role
+
+        public RoleController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var Roles = _context.Roles.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            _context.Roles.Add(Role);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
